83ddcbca78ff57215deacf3b3489fe50c5fc583e
ianbanderas <ian.banderas.alu@iescampanillas.com>
2023-06-21 03:54:56 +0200

    a

--- resources/lang/es/message.php
+++ resources/lang/es/message.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
     return [ 
         "language"=>"idioma",
@@ -44,51 +43,4 @@
 
     ]
 
-=======
-<?php 
-    return [ 
-        "language"=>"idioma",
-        "logOut"=>"salir",
-        "profile"=>"perfil",
-        //login   
-        "Register"=> "Registrarse",
-        "Name"=> "Nombre",
-        "Password" => "Contraseña",
-        "Remember me" => "Recuerdame",
-        "Forgot your password?" => "¿Olvidó su contraseña?",
-        "Log in" => "Entrar",
-        "Confirm Password" => "Confirmar Contraseña",        
-        "Already registered?" => "¿Ya registrado?",
-        "wrong user or password" => "Usuario o contraseña incorrectos",
-        
-        "Next Page" => "Avanzar Pag.",
-        "Previous Page" => "Retroceder Pag",
-        "find" => "Buscar",
-        "category" => "Categoría",
-        "city" => "Ciudad",
-        "home" => "Inicio",
-        "NameUsu" => "Usuario:",
-        "NameRes" => "Restaurante",
-        "add" => "añadir",
-        "addRes" => "añadir Restaurante",
-        "resPass" => "Cambiar Contraseña",
-        "send" => "enviar",
-        "newpass" => "Nueva Contraseña",
-        "addPla" => "Añadir Plato",
-        "Desc" => "Descripción",
-        "menu" => "menú",
-        "Comment" => "Comentario",
-        "delete" => "Borrar",
-        "see" => "Ver Plato",
-        "sorry" => "lo sentimos no hay restaurantes en su zona, múdese o cocine"
-
-        
-
-
-
-
-
-    ]
-
->>>>>>> 38c3dcfc822b5adc5c27022aeae1b0c0709fc263
 ?>
